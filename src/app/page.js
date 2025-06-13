e308c5f8d99804316a75f5c4f206579d8fe4f1f7
chiyuwu <chiyuwu23@gmail.com>
2025-06-13 20:29:30 +0800

    deploy lotus eletre 900v

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ import {
   ResponsiveContainer,
   TooltipProps,
 } from "recharts";
-import response from "./data/None_1004_gemini-2.5-pro-preview-05-06_20250605_0002_linkerZ0_latest_retry_2.json";
+import response from "./data/None_1004_202506122115_lotus_900v_50_gemini-2.5-pro-preview-05-06_gemini-2.5-flash-preview-05-20.json";
 import TableOfContents from "../../demo/TableOfContents";
 import remarkGfm from "remark-gfm";
 
